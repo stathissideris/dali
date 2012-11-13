8d200b2e4f1b591f12e21b58da1e11866f5d8ea5
Stathis Sideris <sideris@gmail.com>
2012-11-13 23:58:11 +0000

    Removed dependency to the clarity library

--- project.clj
+++ project.clj
@@ -3,6 +3,4 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clarity "0.5.6"]])
-
+  :dependencies [[org.clojure/clojure "1.3.0"]])
