90de68f232efa005f27c7dd070f2af3e939d15a7
Stathis Sideris <stathis@pixelated-noise.com>
2014-08-29 20:21:52 +0100

    First version of the stack layout done

--- src/cljx/dali/geom.clj
+++ src/cljx/dali/geom.clj
@@ -1,4 +1,26 @@
 (ns dali.geom)
 
-(defn translate-point [[x y] [dx dy]]
-  [(+ x dx) (+ y dy)])
+(defn v+
+  "Add two vectors."
+  [[x1 y1] [x2 y2]]
+  [(+ x1 x2) (+ y1 y2)])
+
+(defn v-
+  "Subtract the second vector from the first."
+  [[x1 y1] [x2 y2]]
+  [(- x1 x2) (- y1 y2)])
+
+(defn v-neg
+  "Negate vector."
+  [[x y]]
+  [(- x) (- y)])
+
+(defn v-scale
+  "Scale vector."
+  [[x y] a]
+  [(* x a) (* y a)])
+
+(defn v-half
+  "Scale vector to half its magnitude."
+  [v]
+  (v-scale v 0.5))
--- src/cljx/dali/layout.clj
+++ src/cljx/dali/layout.clj
@@ -3,12 +3,12 @@
             [retrograde :as retro]
             [dali.syntax :as s]
             [dali.batik :as batik]
-            [dali.geom :as geom]))
+            [dali.geom :as geom :refer [v+ v- v-half]]))
 
 (defn- replace-blanks [element replacement]
   (walk/postwalk (fn [f] (if (= f :_) replacement f)) element))
 
-(defn- set-top-left
+(defn set-top-left
   "Adds a translation transform to an element so that its top-left
   corner is at the passed position."
   [element top-left bounds]
@@ -16,15 +16,14 @@
         [_ current-pos [w h]] bounds
         transform
         (condp = type
-          :circle (-> current-pos
-                      (geom/translate-point top-left)
-                      (geom/translate-point [(/ w 2) (/ h 2)]))
+          :circle (v+ top-left (v-half [w h]))
           :text top-left ;;TODO
-          (geom/translate-point current-pos top-left))]
+          (v- top-left current-pos))]
     (s/add-transform element [:translate transform])))
 
 (defn stack [ctx {:keys [position direction gap] :as params} & elements]
   (let [gap (or gap 2)
+        [x y] position
         elements (map #(replace-blanks % [0 0]) elements)]
     (into [:g]
      (retro/transform
@@ -32,10 +31,8 @@
        bounds nil (batik/rehearse-bounds ctx element)
        size 0 (let [[_ _ [_ h]] bounds] h)
        pos 0 (let [[_ [_ y] _] bounds] y)
-       this-pos 0 (+ this-pos' size' this-gap')
-       element (s/add-transform
-                element
-                [:translate (geom/translate-point position [0 this-pos])])]
+       this-pos y (+ this-pos' size' this-gap')
+       element (set-top-left element [x this-pos] bounds)]
       elements))))
 
 (comment
@@ -76,17 +73,32 @@
 
 (comment
   (def ctx (batik/batik-context (batik/parse-svg-uri "file:///s:/temp/svg.svg") :dynamic? true))
-  (s/spit-svg
-   (s/dali->hiccup
-    [:page
-     {:height 500 :width 500, :stroke {:paint :black :width 1} :fill :none}
-     [:circle [0 0] 10]
-     (stack
-      ctx
-      {:position [50 50] :gap 5}
-      [:rect :_ [10 100]]
-      [:circle :_ 20]
-      [:rect :_ [10 30]]
-      [:rect :_ [10 5]]
-      [:rect :_ [10 5]])])
-   "s:/temp/svg_stack1.svg"))
+  (let [circle-pos [200 50]]
+   (s/spit-svg
+    (s/dali->hiccup
+     [:page
+      {:height 500 :width 500, :stroke {:paint :black :width 1} :fill :none}
+
+      [:rect circle-pos [100 100]]
+      (set-top-left
+       [:circle [0 0] 50]
+       circle-pos
+       (batik/rehearse-bounds ctx [:circle [0 0] 50]))
+
+      [:circle [200 200] 5]
+      (set-top-left
+       [:rect [-100 -100] [50 50]]
+       [200 200]
+       (batik/rehearse-bounds ctx [:rect [-100 -100] [50 50]]))
+      
+      [:circle [50 50] 5]
+      (stack
+       ctx
+       {:position [50 50] :gap 5}
+       [:rect :_ [10 100]]
+       [:circle :_ 20]
+       [:rect :_ [10 30]]
+       [:rect :_ [10 5]]
+       [:rect :_ [10 5]])])
+    "s:/temp/svg_stack1.svg"))
+  )
