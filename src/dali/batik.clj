6f48f2f4ae826abb4f906ac534269c3392eda94d
Stathis Sideris <stathis@pixelated-noise.com>
2015-12-05 22:30:36 +0200

    Trying to max-out the quality of the Batik PNG transcoder

--- src/dali/batik.clj
+++ src/dali/batik.clj
@@ -3,15 +3,20 @@
             [clojure.string :as string]
             [dali
              [dom :as dom]
-             [syntax :as s]])
-  (:import [java.awt.geom AffineTransform PathIterator]
+             [syntax :as s]]
+            [dali.utils :as utils])
+  (:import [java.awt RenderingHints Rectangle]
+           [java.awt.geom AffineTransform PathIterator]
+           [javax.imageio ImageIO]
+           [java.io File]
            java.io.ByteArrayInputStream
            java.nio.charset.StandardCharsets
-           [org.apache.batik.bridge BridgeContext GVTBuilder UserAgentAdapter]
            [org.apache.batik.anim.dom SAXSVGDocumentFactory SVGDOMImplementation]
-           [org.apache.batik.transcoder TranscoderInput TranscoderOutput]
+           [org.apache.batik.bridge BridgeContext GVTBuilder UserAgentAdapter]
            [org.apache.batik.bridge TextNode]
            [org.apache.batik.gvt RootGraphicsNode CompositeGraphicsNode ShapeNode]
+           [org.apache.batik.gvt.renderer ConcreteImageRendererFactory]
+           [org.apache.batik.transcoder TranscoderInput TranscoderOutput]
            org.apache.batik.transcoder.image.PNGTranscoder))
 
 ;;Batik - calculating bounds of cubic spline
@@ -129,12 +134,49 @@
     (with-open [in (ByteArrayInputStream. (.getBytes s StandardCharsets/UTF_8))]
       (.createDocument factory "file:///fake.svg" in))))
 
+(defn high-quality-png-transcoder []
+  (proxy [PNGTranscoder] []
+    (createRenderer []
+      (let [add-hint (fn [hints k v] (.add hints (RenderingHints. k v)))
+            renderer (proxy-super createRenderer)
+            ;;hints    (.getRenderingHints renderer)
+            hints (RenderingHints. RenderingHints/KEY_ALPHA_INTERPOLATION RenderingHints/VALUE_ALPHA_INTERPOLATION_QUALITY)
+            ]
+        (doto hints
+          (add-hint RenderingHints/KEY_ALPHA_INTERPOLATION RenderingHints/VALUE_ALPHA_INTERPOLATION_QUALITY)
+          (add-hint RenderingHints/KEY_INTERPOLATION       RenderingHints/VALUE_INTERPOLATION_BICUBIC)
+          (add-hint RenderingHints/KEY_ANTIALIASING        RenderingHints/VALUE_ANTIALIAS_ON)
+          (add-hint RenderingHints/KEY_COLOR_RENDERING     RenderingHints/VALUE_COLOR_RENDER_QUALITY)
+          (add-hint RenderingHints/KEY_DITHERING           RenderingHints/VALUE_DITHER_DISABLE)
+          (add-hint RenderingHints/KEY_RENDERING           RenderingHints/VALUE_RENDER_QUALITY)
+          (add-hint RenderingHints/KEY_STROKE_CONTROL      RenderingHints/VALUE_STROKE_PURE)
+          (add-hint RenderingHints/KEY_FRACTIONALMETRICS   RenderingHints/VALUE_FRACTIONALMETRICS_ON)
+          (add-hint RenderingHints/KEY_TEXT_ANTIALIASING   RenderingHints/VALUE_TEXT_ANTIALIAS_OFF))
+        (.setRenderingHints renderer hints)
+        renderer))))
+
 (defn render-document-to-png [svg-document png]
   (with-open [out-stream (io/output-stream (io/file png))]
     (let [in (TranscoderInput. svg-document)
           out (TranscoderOutput. out-stream)]
-      (doto (PNGTranscoder.)
+      (doto (high-quality-png-transcoder)
         (.transcode in out)))))
+
+(comment ;;alternative implementation taken from dali, half broken
+ (defn render-document-to-png [svg-document png]
+   (let [renderer (-> (ConcreteImageRendererFactory.) .createStaticImageRenderer)
+         builder  (GVTBuilder.)
+         ctx      (BridgeContext. (UserAgentAdapter.))]
+     (.setDynamicState ctx BridgeContext/STATIC)
+     (let [root-node (.build builder ctx svg-document)
+           w         (-> ctx .getDocumentSize .getWidth)
+           h         (-> ctx .getDocumentSize .getHeight)]
+       (.setTree renderer root-node)
+       (doto renderer
+         (.updateOffScreen w h)
+         (.setTree root-node)
+         (.repaint (Rectangle. 0 0 w h)))
+       (ImageIO/write (.getOffScreen renderer) "png" (File. png))))))
 
 (defn render-uri-to-png [uri png-filename]
   (render-document-to-png (parse-svg-uri uri) png-filename))
