68de92697d05258fcde22ad76736db7072fbd19f
Stathis Sideris <stathis@pixelated-noise.com>
2016-06-02 12:03:29 +0300

    spec: Constrain numbers to not infinite and not NaN

--- src/dali/spec.clj
+++ src/dali/spec.clj
@@ -6,36 +6,40 @@
   (s/or :defined spec
         ::blank #{:_}))
 
+(s/def ::number (s/and number?
+                       #(not (or (Double/isInfinite %)
+                                 (Double/isNaN %)))))
+
 (let [op (fn [op params-spec]
            (s/cat :op #{op} :params params-spec))]
 
   (s/def ::transform
     (s/+
      (s/alt
-      :matrix-op (op :matrix (s/tuple number? number? number? number? number? number?))
+      :matrix-op (op :matrix (s/tuple ::number ::number ::number ::number ::number ::number))
       :translate-op (op :translate ::point)
-      :scale-op (op :scale (s/or :factor ::point, :factor number?))
-      :rotate-op (op :rotate (s/or :angle number?, :center (s/cat
-                                                               :angle number?
-                                                               :x number?
-                                                               :y number?)))
-      :skew-x-op (op :skew-x number?)
-      :skew-y-op (op :skew-y number?)))))
+      :scale-op (op :scale (s/or :factor ::point, :factor ::number))
+      :rotate-op (op :rotate (s/or :angle ::number, :center (s/cat
+                                                             :angle ::number
+                                                             :x ::number
+                                                             :y ::number)))
+      :skew-x-op (op :skew-x ::number)
+      :skew-y-op (op :skew-y ::number)))))
 
 (s/def ::attr-map
   (s/and (s/keys :opt-un [::transform])
          (s/map-of (s/or :k keyword? :k string?) ::s/any)))
 
-(s/def ::point (s/spec (s/cat :x number? :y number?)))
+(s/def ::point (s/spec (s/cat :x ::number :y ::number)))
 
-(s/def ::dimensions (s/spec (s/cat :w number? :h number?)))
+(s/def ::dimensions (s/spec (s/cat :w ::number :h ::number)))
 
 (defn- boolean? [x] (instance? Boolean x))
 (s/def ::boolean (s/with-gen boolean? #(gen/boolean)))
 
 (s/def ::path-arc-spec
   (s/cat :pos1 ::point
-         :x-axis-rotation number?
+         :x-axis-rotation ::number
          :large-arc-flag ::boolean
          :sweep-flag ::boolean
          :pos2 ::point))
@@ -50,10 +54,10 @@
      :move-by-op (op :move-by :m ::point)
      :line-to-op (op :line-to :L ::point)
      :line-by-op (op :line-by :l ::point)
-     :horizontal-to-op (op :horizontal-to :H number?)
-     :horizontal-by-op (op :horizontal-by :h number?)
-     :vertical-to-op (op :vertical-to :V number?)
-     :vertical-by-op (op :vertical-by :v number?)
+     :horizontal-to-op (op :horizontal-to :H ::number)
+     :horizontal-by-op (op :horizontal-by :h ::number)
+     :vertical-to-op (op :vertical-to :V ::number)
+     :vertical-by-op (op :vertical-by :v ::number)
      :cubic-to-op (op :cubic-to :C (s/cat :p1 ::point :p2 ::point :p3 ::point))
      :cubic-by-op (op :cubic-by :c (s/cat :p1 ::point :p2 ::point :p3 ::point))
      :symmetrical-to-op (op :symmetrical-to :S (s/cat :p1 ::point :p2 ::point))
@@ -95,14 +99,14 @@
          (s/? ;;both or none
           (s/cat
            :center (blankable ::point)
-           :radius number?))))
+           :radius ::number))))
 
 (s/def ::rect-tag
   (s/cat :tag #{:rect}
          :attrs (s/? ::attr-map)
          :position (blankable ::point)
          :dimensions ::dimensions
-         :rounding (s/? (s/or :uniform number?
+         :rounding (s/? (s/or :uniform ::number
                               :distinct ::dimensions)))) ;;TODO not sure about :distinct
 
 (s/def ::generic-tag
