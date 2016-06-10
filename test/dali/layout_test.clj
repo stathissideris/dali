450aabe8ee1b38c6bb02ba4f87cac7c49f1d726e
Stathis Sideris <stathis@pixelated-noise.com>
2016-06-10 20:12:10 +0300

    Remove obsolete test

--- test/dali/layout_test.clj
+++ test/dali/layout_test.clj
@@ -7,7 +7,6 @@
 (def index-tree @#'dali.layout/index-tree)
 (def de-index-tree @#'dali.layout/de-index-tree)
 (def assoc-in-tree @#'dali.layout/assoc-in-tree)
-(def assoc-in-zipper @#'dali.layout/assoc-in-zipper)
 (def get-in-tree @#'dali.layout/get-in-tree)
 (def update-in-tree @#'dali.layout/update-in-tree)
 (def zipper-point-to-path @#'dali.layout/zipper-point-to-path)
@@ -83,7 +82,7 @@
            (zip/node (zipper-point-to-path (utils/ixml-zipper tree) [0 0 3]))))
     (is (= {:tag :circle :attrs {:cx 7 :cy 30 :r 20 :dali/path [0 1 2]}}
            (zip/node (zipper-point-to-path (utils/ixml-zipper tree) [0 1 2])))))
-  
+
   (deftest test-assoc-in-tree
     (is (= {:tag :svg
             :attrs {:dali/path [0]}
@@ -103,26 +102,6 @@
                {:tag :circle :attrs {:cx 7 :cy 30 :r 20 :dali/path [0 1 2]}}
                {:tag :circle :attrs {:cx 8 :cy 30 :r 20 :dali/path [0 1 3]}}]}]}
            (assoc-in-tree tree [0 0 2] :HERE))))
-
-  (deftest test-assoc-in-zipper
-    (is (= {:tag :svg
-            :attrs {:dali/path [0]}
-            :content
-            [{:tag :g
-              :attrs {:dali/path [0 0]}
-              :content
-              [{:tag :circle :attrs {:cx 1 :cy 30 :r 20 :dali/path [0 0 0]}}
-               {:tag :circle :attrs {:cx 2 :cy 30 :r 20 :dali/path [0 0 1]}}
-               :HERE
-               {:tag :circle :attrs {:cx 4 :cy 30 :r 20 :dali/path [0 0 3]}}]}
-             {:tag :g
-              :attrs {:dali/path [0 1]}
-              :content
-              [{:tag :circle :attrs {:cx 5 :cy 30 :r 20 :dali/path [0 1 0]}}
-               {:tag :circle :attrs {:cx 6 :cy 30 :r 20 :dali/path [0 1 1]}}
-               {:tag :circle :attrs {:cx 7 :cy 30 :r 20 :dali/path [0 1 2]}}
-               {:tag :circle :attrs {:cx 8 :cy 30 :r 20 :dali/path [0 1 3]}}]}]}
-           (zip/root (assoc-in-zipper (utils/ixml-zipper tree) [0 0 2] :HERE)))))
 
   (deftest test-update-in-tree
     (is (= {:tag :svg
